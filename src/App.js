a1876ad2b9c7916ff2fd8f7c2744779c339d8cea
meetparkhiya12 <meetparkhiya4@gmail.com>
2024-06-11 10:13:47 +0530

    Demo React-app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import Demo from './Components/Demo';
+
+function App() {
+  return (
+    <Demo/>
+  );
+}
+
+export default App;
---
+++ src/Components/Demo/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Demo = () => {
+  return (
+    <div style={{marginTop:"20px"}}>Hello World</div>
+  )
+}
+
+export default Demo
