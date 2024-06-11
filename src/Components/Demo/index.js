bbde313d016197f32bd5528c7cdffe2e409e36e2
meetparkhiya12 <meetparkhiya4@gmail.com>
2024-06-11 10:39:34 +0530

    demo

--- src/Components/Demo/index.js
+++ src/Components/Demo/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
 const Demo = () => {
-  return (
-    <div style={{marginTop:"20px"}}><h1>Hello World</h1></div>
-  )
+    return (
+        <div style={{ marginTop: "20px" }}>
+            <h1>Hello World </h1>
+            <h1>Hello World </h1>
+            <h1>Hello World </h1>
+            <h1>Hello World </h1>
+            <h1>Hello World </h1>
+            <h1>Hello World </h1>
+            <h1>Hello World </h1>
+            <h1>Hello World </h1>
+            <h1>Hello World </h1>
+            <h1>Hello World </h1>
+            <h1>Hello World </h1>
+            <h1>Hello World </h1>
+            <h1>Hello World </h1>
+            <h1>Hello World </h1>
+        </div>
+    )
 }
 
 export default Demo
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Demo from './Components/Demo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Demo/>
   </React.StrictMode>
 );
 
